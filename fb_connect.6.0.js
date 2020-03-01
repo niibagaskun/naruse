1192c305665433e371ee45750dc249b29540bf04
niibagaskun <61666449+niibagaskun@users.noreply.github.com>
2020-03-02 03:50:29 +0700

    FB Connect JS Indonesia ver.6.0
    FB Connect JS Indonesia ver.6.0
    By : Bagas

---
+++ fb_connect.6.0.js
@@ -0,0 +1,2 @@
+(function(d,s,id){var js,fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s);js.id = id;js.src = "//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v6.0&appId=562457653823916";fjs.parentNode.insertBefore(js,fjs);}
+(document,'script','facebook-jssdk'));
